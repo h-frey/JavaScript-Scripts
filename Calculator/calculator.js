0774a95aff8a91c82b5a1c69b53200a3a3af1806
Humphrey <hnyanzi32@gmail.com>
2021-11-22 23:41:51 +0300

    Addition of Calculator and other MDN lessons

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,45 +1,67 @@
-let numericalBtns = document.getElementsByClassName("numericalBtns");
-let screenText = document.getElementById("screen");
-let operationBtns = document.getElementsByClassName("operationBtns");
+const numericalBtns = document.getElementsByClassName("numericalBtns");
+const screenText = document.getElementById("screen");
+const operationBtns = document.getElementsByClassName("operationBtns");
+const clearBtn = document.getElementById("clearBtn");
+const evaluateBtn = document.getElementById("evaluateBtn");
+
 let num1 = "";
 let num2 = "";
+let result;
 let operand;
 
 
-function getFirstValue() {
-    for (let i = 0; i < numericalBtns.length; i++) {
-        numericalBtns[i].addEventListener("click", getValue);
+screenText.textContent = 0;
 
-        function getValue() {
-            let num = numericalBtns[i].textContent;
-            num1 += num;
-            screenText.textContent = num1;
-            return num1;
+
+for (let i = 0; i < numericalBtns.length; i++) {
+    numericalBtns[i].addEventListener("click", function() {
+        let num = numericalBtns[i].textContent;
+        num1 += num;
+        screenText.textContent = num1;
+        return num1;
+    })
+}
+
+for (let i = 0; i < operationBtns.length; i++) {
+    operationBtns[i].addEventListener("click", function() {
+        operand = operationBtns[i].textContent;
+        screenText.textContent = "";
+        for (let j = 0; j < numericalBtns.length; j++) {
+            numericalBtns[j].addEventListener("click", function() {
+                let num_ = numericalBtns[j].textContent;
+                num2 += num_;
+                screenText.textContent = num2;
+                return num2;
+            });
         }
-    }
-}
-
-function getSecondValue() {
-    for (let j = 0; j < numericalBtns.length; j++) {
-        numericalBtns[j].addEventListener("click", function() {
-            let num_ = numericalBtns[j].textContent;
-            num2 += num_;
-            screenText.textContent = num2;
-            return num2;
-        });
-    }
-}
-
-function getOperand() {
-    for (let i = 0; i < operationBtns.length; i++) {
-        operationBtns[i].addEventListener("click", function() {
-            operand = operationBtns[i].textContent;
-            screenText.textContent = num2;
-            return operand;
-
-        })
-    }
+        return operand;
+    })
 }
 
 
 
+evaluateBtn.onclick = function() {
+    num1 = num1.replace(num2, " ");
+    result = eval(num1 + operand + num2);
+    screenText.textContent = result;
+
+}
+
+
+clearBtn.onclick = function() {
+    screenText.textContent = 0;
+    num1 = "";
+    num2 = "";
+    result = "";
+    operand = "";
+    window.location.reload();
+}
+
+percentBtn.onclick = function() {
+
+   
+
+}
+deleteBtn.onclick =function() {
+    screenText.textContent = screenText.textContent.slice(0,-1);
+}
